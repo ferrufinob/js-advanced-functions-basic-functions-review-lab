0eb7dc3c63247b20a012471d21c69a4cbb7d6461
Brenda Ferrufino <ferrufinob@gmail.com>
2021-03-17 17:31:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Your code here
+// Function Declaration - Regular Function
+// loads before any code is executed.
+function saturdayFun(string = "roller-skate") {
+  return `This Saturday, I want to ${string}!`;
+}
+
+// Function Expression - anonymous function inside of a variable.
+// loads only when the interpreter reaches that line of code.
+let mondayWork = function (string = "go to the office") {
+  return `This Monday, I will ${string}.`;
+};
+
+function wrapAdjective(arg = "*") {
+  return function (string = "special") {
+    return `You are ${arg}${string}${arg}!`;
+  };
+}
+
+const Calculator = {
+  add: function (x, y) {
+    return x + y;
+  },
+  subtract: function (x, y) {
+    return x - y;
+  },
+  multiply: function (x, y) {
+    return x * y;
+  },
+  divide: function (x, y) {
+    return x / y;
+  },
+};
+
+function actionApplyer(integer, arr) {
+  if (arr === []) {
+    return integer;
+  } else {
+    return arr.reduce(function (int, func) {
+      return func(int);
+    }, integer);
+  }
+}
